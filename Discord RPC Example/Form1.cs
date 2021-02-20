ceee83bc9a1eaab45d232277d46a9b0d319df22e
MatrixPanel <79344045+MatrixPanel@users.noreply.github.com>
2021-02-19 23:01:04 -0500

    Added The Source

---
+++ Discord RPC Example/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRPC;
+using DiscordRPC.Logging;
+//Coded and supplied by Matrix
+namespace Discord_RPC_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void err()
+        {
+            MessageBox.Show("Error has occured", "Error");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DiscordRpcClient Matrix = new DiscordRpcClient("Input your Client ID for application here");//Go to https://discord.com/developers/applications and create a application and copy the client id and paste it in here
+                Matrix.Initialize();
+                Matrix.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+                if (Matrix.IsInitialized == false)
+                {
+                    err();
+                }
+                else
+                {
+                    Matrix.SetPresence(new DiscordRPC.RichPresence()//Setting the Presence
+                    {
+                        Details = $"Developing",//This displays under your application name 
+                        State = $"Matrix Geo Bot",//this goes under Details
+                        Timestamps = Timestamps.Now,//Starts counting how long you have been playing the application
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = $"anime",//Picture that shows when playing Set this in the application panel
+                            LargeImageText = $"Come Cop Matrix Panel",//Text that shows when you hover over the picture
+                        }
+                    });
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Discord RPC Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Discord RPC Example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Discord RPC Example")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c723fa4-c978-42e1-b37a-34d98d779bba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
